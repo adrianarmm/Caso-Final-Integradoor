fdf1e82cdc9c284ecdc9b745da4b8d8ea82a1b80
adrianarmm <areyemor@myuax.com>
2023-11-30 11:51:59 +0100

    3

--- cfi.cpp
+++ cfi.cpp
@@ -5,3 +5,7 @@
 void load_script(const char* filename, bool show_script = false) {
     std::ifstream file(filename);
 
+    if (!file.is_open()) {
+        std::cout << "No se pudo abrir el archivo " << filename << ".\n";
+        return;
+    }
